dd00accf51299b93bacc2254e8b9db147d50ea15
Dener Schenatto Moret <dener1307@gmail.com>
2024-05-20 19:42:06 -0300

    carrosel
    adicionei um carrosel inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000, // Define o tempo de atraso em milissegundos (3 segundos)
+            disableOnInteraction: false, // Continua o autoplay após interação do usuário
+        },
+    });
+});
